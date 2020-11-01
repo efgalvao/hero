e11e0093e6310f74b3f7c1e63ea1a72b122c7e50
efgalvao <solfieri@hotmail.com>
2020-10-31 21:55:16 -0300

    Some minor adjustments in pages.

--- app/models/persona.rb
+++ app/models/persona.rb
@@ -1,4 +1,8 @@
 class Persona < ApplicationRecord
     mount_uploader :picture, PictureUploader
     has_many :comments
+    validates :name, presence: true
+    validates :karma, presence: true
+    validates :media, presence: true
+    validates :appearance, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  root 'pages#home'
   resources :comments
   get 'pages/info'
   resources :personas
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: redirect('/personas')
 end
